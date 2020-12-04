cdae310ec2b8184a094973e6fe357e7dce425038
saoussen <sawsan_sli3i@live.fr>
2020-12-04 15:04:36 +0000

    refactoring

--- index.js
+++ index.js
@@ -2,38 +2,22 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    logState: function() {
+      console.log(`${this.title} has ${this.complete === true ? " " : " not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
 }
 
-// DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
-
-// for now, let's just make sure we see our tasks
-
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-
-// marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
-}
-
-// DRIVER CODE BELOW
-
-const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
-
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
 console.log(tasks);
+task1.logState();
+task1.markCompleted();
+task1.logState();
