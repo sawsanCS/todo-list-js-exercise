5428c2085db30e0bab0c6e75e1fc43d0b054ddb8
saoussen <sawsan_sli3i@live.fr>
2020-12-04 14:59:16 +0000

    refactored the other methods

--- index.js
+++ index.js
@@ -17,22 +17,23 @@ const tasks = [task1, task2];
 
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
 }
 
 // DRIVER CODE BELOW
 
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
 
 console.log(tasks);
-
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
